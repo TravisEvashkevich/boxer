42e6d90208ba307fcb824d49cdecaafaa0f46810
Travis Evashkevich <smackmeawake@gmail.com>
2014-05-15 13:27:58 +0200

    initial comboBox with folder names for folders/imagedata

--- src/Sprite Utility/ViewModel/FolderViewVM.cs
+++ src/Sprite Utility/ViewModel/FolderViewVM.cs
@@ -1,26 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Boxer.Core;
 using Boxer.Data;
+using Microsoft.Practices.ServiceLocation;
 
 namespace Boxer.ViewModel
 {
     public class FolderViewVM : MainViewModel
     {
         private Folder _folder;
+        private ObservableCollection<Folder> _folderList;
 
         public Folder Folder
         {
-            get { return _folder; }
-            set { Set(ref _folder, value); }
+            get
+            {
+                return _folder;
+            }
+            set
+            {
+                if (_folder != value)
+                {
+                    Set(ref _folder, value);
+                    var temp = new ObservableCollection<Folder>();
+                    foreach (Folder folders in ServiceLocator.Current.GetInstance<MainWindowVM>().Documents[0].Children)
+                    {
+                        FindFolders(temp, new Stack<NodeWithName>(), folders);
+                    }
+                    FoldersList = temp;
+                    return;
+                }
+                Set(ref _folder, value);
+                
+                
+            }
+        }
+
+        //Stuff for storing/changing parent of image/folders
+        public ObservableCollection<Folder> FoldersList
+        {
+            get
+            {
+                foreach (Folder folders in ServiceLocator.Current.GetInstance<MainWindowVM>().Documents[0].Children)
+                {
+                    FindFolders(_folderList, new Stack<NodeWithName>(),
+                    folders);
+                }
+
+
+                return _folderList;
+            }
+            set { Set(ref _folderList, value); }
+        }
+
+        public void FindFolders(ObservableCollection<Folder> allFolders, Stack<NodeWithName> ancestors, NodeWithName startPoint)
+        {
+            ancestors.Push(startPoint);
+            if (startPoint.Children != null && startPoint.Children.Count > 0)
+            {
+                foreach (var child in startPoint.Children)
+                {
+                    if (child.Type == "Folder" && !FolderIsChildOf(Folder,child as Folder) && !child.Name.Contains(Folder.Name))
+                    {
+                        allFolders.Add(child as Folder);
+                        FindFolders(allFolders, ancestors, child as NodeWithName);
+                    }
+                }
+            }
+
+            ancestors.Pop();
+        }
+
+        private bool FolderIsChildOf(Folder parentFolder, Folder childFolder)
+        {
+            foreach (var child in parentFolder.Children)
+            {
+                if (child is Folder && child.Name == childFolder.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         protected override void InitializeCommands()
         {
-
+            _folderList = new ObservableCollection<Folder>();
         }
     }
 }
--- src/Sprite Utility/ViewModel/ImageViewVM.cs
+++ src/Sprite Utility/ViewModel/ImageViewVM.cs
@@ -1,5 +1,8 @@
-﻿using Boxer.Core;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Boxer.Core;
 using Boxer.Data;
+using Microsoft.Practices.ServiceLocation;
 
 namespace Boxer.ViewModel
 {
@@ -11,6 +14,36 @@ namespace Boxer.ViewModel
         {
             get { return _image; }
             set { Set(ref _image, value); }
+        }
+
+        //Stuff for storing/changing parent of image/folders
+        public ObservableCollection<Folder> FoldersList
+        {
+            get
+            {
+                var names = new ObservableCollection<Folder>();
+
+                FindFolders(names, new Stack<NodeWithName>(),
+                    ServiceLocator.Current.GetInstance<MainWindowVM>().Documents[0]);
+
+                return names;
+            }
+        }
+
+        public void FindFolders(ObservableCollection<Folder> allFolders, Stack<NodeWithName> ancestors, NodeWithName startPoint)
+        {
+            ancestors.Push(startPoint);
+            if (startPoint.Children != null && startPoint.Children.Count > 0)
+            {
+                foreach (var child in startPoint.Children)
+                    if (child.Type == "Folder")
+                    {
+                        allFolders.Add(child as Folder);
+                        FindFolders(allFolders, ancestors, child as NodeWithName);
+                    }
+            }
+
+            ancestors.Pop();
         }
 
         protected override void InitializeCommands()
